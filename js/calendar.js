7edbe36d2d27d685791074f0dd305d4a91631acb
jnkiarie <92355202+jnkiarie@users.noreply.github.com>
2021-11-26 14:48:08 +0300

    add prompt methods to capture user data

--- js/calendar.js
+++ js/calendar.js
@@ -1,2 +1,15 @@
-alert("Enter Your date of Birth");
-day = parseInt(prompt("Enter the day you were born"));
+
+    alert("Enter Your date of Birth");
+    var DD = parseInt(prompt("Enter the day you were born:"));
+    var MM = parseInt(prompt("Enter the month you were born:"));
+    var year = (prompt("Enter the year you were born:"))
+    var gender = prompt("Enter your Gender M or F");
+    var centuryyear = year.split("");
+    var YY = parseInt(centuryyear.splice(2).join(''));
+    CC = parseInt(centuryyear.join(''));
+    
+    var d = Math.floor((centuryDigitsCC(CC) + yearDigitsYY(YY) + month(MM) + DD)%7);
+    
+    
+    
+    
